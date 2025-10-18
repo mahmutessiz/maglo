cefed9d9f95451e572d6cca64350f33bc86a436e
mahmutessiz <mahmutessiz71@gmail.com>
2025-10-18 16:32:33 +0300

    fix: declare css modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Kumbh_Sans } from "next/font/google";
 import QueryProvider from "../components/providers/QueryProvider";
 import { Toaster } from "react-hot-toast";
-// @ts-expect-error Missing types
+
 import "./globals.css";
 
 const geistSans = Geist({
